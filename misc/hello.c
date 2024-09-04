223b1a05b2a8976f487c0e206a0c13041ec3bde2
JosephCasey3 <jcasey14@huskers.unl.edu>
2024-09-03 18:39:11 -0700

    initial commit

---
+++ misc/hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Joseph Casey
+ * Date: 2024/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
+
